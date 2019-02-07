ca66ef2fe5c29f144dc79488d8ee4c67fc6f092d
DataCollectingTami <tamirisrd@hotmail.com>
2019-02-07 16:49:25 +0100

    input box text updates

--- src/main/java/crisscrosscrass/InfoText.java
+++ src/main/java/crisscrosscrass/InfoText.java
@@ -9,13 +9,13 @@ public enum InfoText {
             "Pick a brand which has series so that Series Filter Box can also be tested. \n" ),
     FilterText("Filer Buttons","This field will be used to test a gridpage with Windows\n"),
     GridPageWitthFillIns("Grid Page URL with FillIns","This field will be used to test a gridpage with Fill-ins\n"),
-    exclamationMarkGridPageURLwithoutWindows("Info","- Grid page has to have a style box with more than 6 tags\n (or it has to be a tag with at least 6 children)\n" +
+    exclamationMarkGridPageURLwithoutWindows("Info","- Grid page has to have a style box with more than 4 tags\n (or it has to be a tag with at least 6 children)\n" +
             "- It cannot be a one-gender tag\n" +
             "- Grid page has to contain items from chosen brands and shops\n"),
     LucenePageSearch("Keyword trigger Lucene Page","This keyword will be used to trigger a Lucene page\n " +
             "and perform all the Lucene tests. \n"),
-    ShopSearchBox("Shop Search Box","This field will be used to test search on Partnershop page and Grid Page.\n " +
-            "If you enter more than one Shop, use pipe as separator.\n"),
+    ShopSearchBox("Shop Search Box","This field will be used to test search on Partnershop page and Grid Page.\n "),
+            //"If you enter more than one Shop, use pipe as separator.\n"),
     MerchandiseSearch("Merchandise Search Box","This field will be used to test search on Merchandise page.\n " +
             "If you enter more than one Merchandise, use pipe as separator.\n");
     private String HeaderMessage;
