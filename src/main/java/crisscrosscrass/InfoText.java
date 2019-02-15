8d4343160ac251cd6c00e5a19e98af43f8145be2
DataCollectingTami <tamirisrd@hotmail.com>
2019-02-15 12:03:24 +0100

    minor adjustments in infomessage

--- src/main/java/crisscrosscrass/InfoText.java
+++ src/main/java/crisscrosscrass/InfoText.java
@@ -9,7 +9,7 @@ public enum InfoText {
             "Pick a brand which has series so that Series Filter Box can also be tested. \n" ),
     FilterText("Filer Buttons","This field will be used to test a gridpage with Windows\n"),
     GridPageWitthFillIns("Grid Page URL with FillIns","This field will be used to test a gridpage with Fill-ins\n"),
-    exclamationMarkGridPageURLwithoutWindows("Info","- Grid page has to have a style box with more than 4 tags\n (or it has to be a tag with at least 6 children)\n" +
+    exclamationMarkGridPageURLwithoutWindows("Info","- Grid page has to have a style box with more than 4 tags\n (or it has to be a tag with at least 4 children)\n" +
             "- It cannot be a one-gender tag\n" +
             "- Grid page has to contain items from chosen brands and shops\n"),
     LucenePageSearch("Keyword trigger Lucene Page","This keyword will be used to trigger a Lucene page\n " +
